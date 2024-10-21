21e7a50be5a2d9e502598e37906a7188ea1e356a
DawidP2001 <c00273530@setu.ie>
2024-10-21 13:46:38 +0100

    Changes to 2 first labs

--- Lab02-Rendezvous/go.mod
+++
@@ -1,3 +0,0 @@
-module Lab02/rendezvous
-
-go 1.23.2
--- Lab02-Rendezvous/rendezvous.go
+++ Lab02-Rendezvous/rendezvous.go
@@ -2,34 +2,40 @@ package main
 
 import (
 	"fmt"
+	"math/rand/v2"
 	"sync"
 	"time"
-	"math/rand/v2"
 )
+
 //Global variables shared between functions --A BAD IDEA
 
-func WorkWithRendezvous(wg *sync.WaitGroup, Num int) bool {
+func WorkWithRendezvous(wg *sync.WaitGroup, Num int, barrierSem chan bool, arrived *int) bool {
 	var X time.Duration
-	X=time.Duration(rand.IntN(5))
-	time.Sleep(X * time.Second)//wait random time amount
+	X = time.Duration(rand.IntN(5))
+	time.Sleep(X * time.Second) //wait random time amount
 	fmt.Println("Part A", Num)
 	//Rendezvous here
-
-	fmt.Println("PartB",Num)
+	*arrived++
+	if *arrived == 5 {
+		barrierSem <- true
+		<-barrierSem
+	} else {
+		<-barrierSem
+		barrierSem <- true
+	}
+	fmt.Println("PartB", Num)
 	wg.Done()
 	return true
 }
 
-
-
 func main() {
 	var wg sync.WaitGroup
-	//barrier := make(chan bool)
-	threadCount:=5
-
+	barrierSem := make(chan bool)
+	threadCount := 5
+	arrived := 0
 	wg.Add(threadCount)
 	for N := range threadCount {
-		go WorkWithRendezvous(&wg, N)
+		go WorkWithRendezvous(&wg, N, barrierSem, &arrived)
 	}
 	wg.Wait() //wait here until everyone (10 go routines) is done
 
